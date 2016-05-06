18d08005459adfd90ac8834e91079f364bb809b5
Jochen Klar <jochenklar@gmail.com>
2016-05-06 10:43:24 +0200

    add select form quantities and date

--- static/data.js
+++ static/data.js
@@ -9,7 +9,30 @@ app.config(['$httpProvider', '$interpolateProvider', function($httpProvider, $in
 
 app.factory('DataService', ['$http','$window',function($http,$window) {
 
-    var service = {};
+    var service = {
+        'quantities': [
+            {
+                key: 'magnitude',
+                text: 'Magnitude'
+            },
+            {
+                key: 'frequency',
+                text: 'Frequenz'
+            },
+            {
+                key: 'counts',
+                text: 'Counts'
+            },
+            {
+                key: 'period',
+                text: 'Periode'
+            },
+            {
+                key: 'temperature',
+                text: 'Temperatur'
+            }
+        ]
+    };
 
     var urls = {
         'locations': '/api/locations/',
@@ -18,28 +41,40 @@ app.factory('DataService', ['$http','$window',function($http,$window) {
 
     function getDate() {
         var date = new Date();
-
-        // if (date.getHours() > 12) {
-        //     date.setDate(date.getDate() - 1);
-        // }
-        date.setHours(12, 0, 0);
+        date.setDate(date.getDate() - 1);
+        date.setHours(18, 0, 0);
         return date;
     }
 
-    function fetchLocations() {
-        $http.get(urls.locations).success(function(response) {
-            service.locations = response;
-
-            if (angular.isUndefined(service.location)) {
-                service.location = service.locations[0];
-                fetchMeasurements();
-            }
-        });
+    function getMin(data, key) {
+        return data.reduce(function(prev, curr) {
+            return prev[key] < curr[key] ? prev : curr;
+        })[key];
     }
 
-    function fetchMeasurements() {
+    function getMax(data, key) {
+        return data.reduce(function(prev, curr) {
+            return prev[key] > curr[key] ? prev : curr;
+        })[key];
+    }
+
+    service.init = function() {
+        service.date = getDate();
+        service.quantity = 'magnitude';
+
+        // fetch locations
+        $http.get(urls.locations).success(function(response) {
+            service.locations = response;
+            service.location = service.locations[0];
+
+            service.fetchMeasurements();
+        });
+    };
+
+    service.fetchMeasurements = function() {
         var before = angular.copy(service.date);
         before.setDate(before.getDate() + 1);
+        before.setHours(8, 0, 0);
 
         var config = {
             params: {
@@ -51,39 +86,43 @@ app.factory('DataService', ['$http','$window',function($http,$window) {
 
         $http.get(urls.measurements, config).success(function(response) {
             service.measurements = response;
-            drawPlot();
-        });
-    }
 
-    function drawPlot() {
+            service.drawPlot();
+        });
+    };
+
+    service.drawPlot = function() {
+
+        d3.selectAll("svg > *").remove();
+
+        var data = service.measurements,
+            key = service.quantity;
+
+        if (data.length === 0) return;
 
         var margin = {top: 10, right: 10, bottom: 30, left: 50},
             width = 640 - margin.left - margin.right,
             height = 480 - margin.top - margin.bottom;
 
-        var xMin = service.date;
-        var xMax = angular.copy(service.date);
-        xMax.setDate(xMax.getDate() + 1);
+        var xMin = new Date(getMin(data, 'timestamp')),
+            xMax = new Date(getMax(data, 'timestamp')),
+            yMin = Math.ceil(getMax(data, key)),
+            yMax = getMin(data, key);
 
-        var yMin = 20,
-            yMax = 60;
+        var xScale = d3.time.scale.utc().domain([xMin, xMax]).range([0, width]),
+            yScale = d3.scale.linear().domain([yMin, yMax]).range([height, 0]);
 
-        var xScale = d3.time.scale.utc()
-                        .domain([xMin, xMax])
-                        .range([0, width]),
-            yScale = d3.scale.linear()
-                        .domain([yMin, yMax])
-                        .range([height, 0]);
-
-        var ticks = d3.time.hours,
-            tickFormat = d3.time.format('%H');
+        var xTicks = d3.time.hours,
+            xTickFormat = d3.time.format('%H:00');
 
         var xAxis = d3.svg.axis().scale(xScale)
                         .orient('bottom')
-                        .ticks(ticks)
-                        .tickFormat(tickFormat),
+                        .ticks(xTicks)
+                        .tickFormat(xTickFormat),
             yAxis = d3.svg.axis().scale(yScale)
                         .orient('left');
+
+        d3.selectAll("svg > *").remove();
 
         var svg = d3.select('#plot')
                         .append("g")
@@ -96,30 +135,14 @@ app.factory('DataService', ['$http','$window',function($http,$window) {
             .attr('class', 'axis')
             .attr('transform', 'translate(0, 0)');
 
-        // svg.append('g').selectAll('circle')
-        //     .data(service.measurements)
-        //     .enter()
-        //         .append('circle')
-        //         .attr('class', 'data')
-        //         .attr('cx', function (d) { return xScale(new Date(d.timestamp)); })
-        //         .attr('cy', function (d) { return yScale(d.temperature); });
-
         var line = d3.svg.line()
             .x(function (d) { return xScale(new Date(d.timestamp)); })
-            .y(function (d) { return yScale(d.temperature); })
+            .y(function (d) { return yScale(d[key]); })
             .interpolate('basis');
 
         svg.append('g').append("path")
-            .attr("d", line(service.measurements))
+            .attr("d", line(data))
             .attr('class', 'data');
-    };
-
-
-    service.init = function() {
-
-        service.date = getDate();
-        fetchLocations();
-
     };
 
     return service;
