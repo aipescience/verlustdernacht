04b0df69ae3ed1d5cd2b6e777236f67549c0c1a1
Jochen Klar <jklar@aip.de>
2016-05-10 11:47:31 +0200

    cleanup

--- verlustdernacht/urls.py
+++ verlustdernacht/urls.py
@@ -6,7 +6,7 @@ from api import urls
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^impressum/$', TemplateView.as_view(template_name='impressum.html')),
+    url(r'^impressum/$', TemplateView.as_view(template_name='impressum.html'), name='impressum'),
     url(r'^data/$', TemplateView.as_view(template_name='data.html'), name='data'),
     url(r'^locations/$', TemplateView.as_view(template_name='locations.html'), name='locations'),
     url(r'^api/', include(urls)),
