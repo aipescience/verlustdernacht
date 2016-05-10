4cb8739da9274e36b93aaf2f29fc90bfbca40e36
Jochen Klar <jklar@aip.de>
2016-05-10 11:01:07 +0200

    use urlnames for links

--- verlustdernacht/urls.py
+++ verlustdernacht/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from api import urls
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^impressum$', TemplateView.as_view(template_name='impressum.html')),
-    url(r'^data$', TemplateView.as_view(template_name='data.html')),
+    url(r'^data$', TemplateView.as_view(template_name='data.html'), name='data'),
+    url(r'^locations$', TemplateView.as_view(template_name='locations.html'), name='locations'),
     url(r'^api/', include(urls)),
     url(r'^admin/', admin.site.urls),
-)
+]
